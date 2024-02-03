df32520b9cd3e3a795793e4907b1a4dda8093344
kaeem elsum <121341685+kareem1234562@users.noreply.github.com>
2024-02-03 19:26:25 +0200

    Add files via upload

---
+++ six/index.js
@@ -0,0 +1,384 @@
+function x1()
+{
+  let x1=document.getElementById("first")
+  let tot=document.getElementById("total1")
+
+  tot.innerHTML=parseInt(x1.value)
+
+}
+function x2()
+{
+  let x1=document.getElementById("first")
+  let x2=document.getElementById("second")
+  let tot=document.getElementById("total1")
+
+
+  tot.innerHTML=parseInt(x1.value)+parseInt(x2.value)
+ 
+
+}
+function x3()
+{
+  let x1=document.getElementById("first")
+  let x2=document.getElementById("second")
+  let x3=document.getElementById("third")
+  let tot=document.getElementById("total1")
+
+tot.innerHTML=parseInt(x1.value)+parseInt(x2.value)+parseInt(x3.value)
+ 
+
+}
+function x4()
+{
+  let x1=document.getElementById("first")
+  let x2=document.getElementById("second")
+  let x3=document.getElementById("third")
+  let x4=document.getElementById("forth")
+  let tot=document.getElementById("total1")
+
+tot.innerHTML=parseInt(x1.value)+parseInt(x2.value)+parseInt(x3.value)+parseInt(x4.value)
+ 
+
+}
+function x5()
+{
+  let x1=document.getElementById("first")
+  let x2=document.getElementById("second")
+  let x3=document.getElementById("third")
+  let x4=document.getElementById("forth")
+  let x5=document.getElementById("fifth")
+  let tot=document.getElementById("total1")
+
+tot.innerHTML=parseInt(x1.value)+parseInt(x2.value)+parseInt(x3.value)+parseInt(x4.value)+parseInt(x5.value)
+}
+
+
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+function x12()
+{
+  let x1=document.getElementById("first2")
+  let tot=document.getElementById("total2")
+
+tot.innerHTML=parseInt(x1.value)
+ 
+
+}
+function x22()
+{
+  let x1=document.getElementById("first2")
+  let x2=document.getElementById("second2")
+  let tot=document.getElementById("total2")
+
+tot.innerHTML=parseInt(x1.value)+parseInt(x2.value)
+ 
+
+}
+function x32()
+{
+  let x1=document.getElementById("first2")
+  let x2=document.getElementById("second2")
+  let x3=document.getElementById("third2")
+  let tot=document.getElementById("total2")
+
+tot.innerHTML=parseInt(x1.value)+parseInt(x2.value)+parseInt(x3.value)
+ 
+
+}
+function x42()
+{
+  let x1=document.getElementById("first2")
+  let x2=document.getElementById("second2")
+  let x3=document.getElementById("third2")
+  let x4=document.getElementById("forth2")
+  let tot=document.getElementById("total2")
+
+tot.innerHTML=parseInt(x1.value)+parseInt(x2.value)+parseInt(x3.value)+parseInt(x4.value)
+ 
+
+}
+function x52()
+{
+  let x1=document.getElementById("first2")
+  let x2=document.getElementById("second2")
+  let x3=document.getElementById("third2")
+  let x4=document.getElementById("forth2")
+  let x5=document.getElementById("fifth2")
+  let tot=document.getElementById("total2")
+
+tot.innerHTML=parseInt(x1.value)+parseInt(x2.value)+parseInt(x3.value)+parseInt(x4.value)+parseInt(x5.value)
+}
+
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+function x13()
+{
+  let x1=document.getElementById("first3")
+  let tot=document.getElementById("total3")
+
+tot.innerHTML=parseInt(x1.value)
+ 
+
+}
+function x23()
+{
+  let x1=document.getElementById("first3")
+  let x2=document.getElementById("second3")
+  let tot=document.getElementById("total3")
+
+tot.innerHTML=parseInt(x1.value)+parseInt(x2.value)
+ 
+
+}
+function x33()
+{
+  let x1=document.getElementById("first3")
+  let x2=document.getElementById("second3")
+  let x3=document.getElementById("third3")
+  let tot=document.getElementById("total3")
+
+tot.innerHTML=parseInt(x1.value)+parseInt(x2.value)+parseInt(x3.value)
+ 
+
+}
+function x43()
+{
+  let x1=document.getElementById("first3")
+  let x2=document.getElementById("second3")
+  let x3=document.getElementById("third3")
+  let x4=document.getElementById("forth3")
+  let tot=document.getElementById("total3")
+
+tot.innerHTML=parseInt(x1.value)+parseInt(x2.value)+parseInt(x3.value)+parseInt(x4.value)
+ 
+
+}
+function x53()
+{
+  let x1=document.getElementById("first3")
+  let x2=document.getElementById("second3")
+  let x3=document.getElementById("third3")
+  let x4=document.getElementById("forth3")
+  let x5=document.getElementById("fifth3")
+  let tot=document.getElementById("total3")
+
+tot.innerHTML=parseInt(x1.value)+parseInt(x2.value)+parseInt(x3.value)+parseInt(x4.value)+parseInt(x5.value)
+}
+
+
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+function x14()
+{
+  let x1=document.getElementById("first4")
+  let tot=document.getElementById("total4")
+
+tot.innerHTML=parseInt(x1.value)
+ 
+
+}
+function x24()
+{
+  let x1=document.getElementById("first4")
+  let x2=document.getElementById("second4")
+  let tot=document.getElementById("total4")
+
+tot.innerHTML=parseInt(x1.value)+parseInt(x2.value)
+ 
+
+}
+function x34()
+{
+  let x1=document.getElementById("first4")
+  let x2=document.getElementById("second4")
+  let x3=document.getElementById("third4")
+  let tot=document.getElementById("total4")
+
+tot.innerHTML=parseInt(x1.value)+parseInt(x2.value)+parseInt(x3.value)
+ 
+
+}
+function x44()
+{
+  let x1=document.getElementById("first4")
+  let x2=document.getElementById("second4")
+  let x3=document.getElementById("third4")
+  let x4=document.getElementById("forth4")
+  let tot=document.getElementById("total4")
+
+tot.innerHTML=parseInt(x1.value)+parseInt(x2.value)+parseInt(x3.value)+parseInt(x4.value)
+ 
+
+}
+function x54()
+{
+  let x1=document.getElementById("first4")
+  let x2=document.getElementById("second4")
+  let x3=document.getElementById("third4")
+  let x4=document.getElementById("forth4")
+  let x5=document.getElementById("fifth4")
+  let tot=document.getElementById("total4")
+
+tot.innerHTML=parseInt(x1.value)+parseInt(x2.value)+parseInt(x3.value)+parseInt(x4.value)+parseInt(x5.value)
+}
+
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+function x15()
+{
+  let x1=document.getElementById("first5")
+  let tot=document.getElementById("total5")
+
+tot.innerHTML=parseInt(x1.value)
+ 
+
+}
+function x25()
+{
+  let x1=document.getElementById("first5")
+  let x2=document.getElementById("second5")
+  let tot=document.getElementById("total5")
+
+tot.innerHTML=parseInt(x1.value)+parseInt(x2.value)
+ 
+
+}
+function x35()
+{
+  let x1=document.getElementById("first5")
+  let x2=document.getElementById("second5")
+  let x3=document.getElementById("third5")
+  let tot=document.getElementById("total5")
+
+tot.innerHTML=parseInt(x1.value)+parseInt(x2.value)+parseInt(x3.value)
+ 
+
+}
+function x45()
+{
+  let x1=document.getElementById("first5")
+  let x2=document.getElementById("second5")
+  let x3=document.getElementById("third5")
+  let x4=document.getElementById("forth5")
+  let tot=document.getElementById("total5")
+
+tot.innerHTML=parseInt(x1.value)+parseInt(x2.value)+parseInt(x3.value)+parseInt(x4.value)
+ 
+
+}
+function x55()
+{
+  let x1=document.getElementById("first5")
+  let x2=document.getElementById("second5")
+  let x3=document.getElementById("third5")
+  let x4=document.getElementById("forth5")
+  let x5=document.getElementById("fifth5")
+  let tot=document.getElementById("total5")
+
+tot.innerHTML=parseInt(x1.value)+parseInt(x2.value)+parseInt(x3.value)+parseInt(x4.value)+parseInt(x5.value)
+}
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+function x16()
+{
+  let x1=document.getElementById("first6")
+  let tot=document.getElementById("total6")
+
+tot.innerHTML=parseInt(x1.value)
+ 
+
+}
+function x26()
+{
+  let x1=document.getElementById("first6")
+  let x2=document.getElementById("second6")
+  let tot=document.getElementById("total6")
+
+tot.innerHTML=parseInt(x1.value)+parseInt(x2.value)
+ 
+
+}
+function x36()
+{
+  let x1=document.getElementById("first6")
+  let x2=document.getElementById("second6")
+  let x3=document.getElementById("third6")
+  let tot=document.getElementById("total6")
+
+tot.innerHTML=parseInt(x1.value)+parseInt(x2.value)+parseInt(x3.value)
+ 
+
+}
+function x46()
+{
+  let x1=document.getElementById("first6")
+  let x2=document.getElementById("second6")
+  let x3=document.getElementById("third6")
+  let x4=document.getElementById("forth6")
+  let tot=document.getElementById("total6")
+
+tot.innerHTML=parseInt(x1.value)+parseInt(x2.value)+parseInt(x3.value)+parseInt(x4.value)
+ 
+
+}
+function x56()
+{
+  let x1=document.getElementById("first6")
+  let x2=document.getElementById("second6")
+  let x3=document.getElementById("third6")
+  let x4=document.getElementById("forth6")
+  let x5=document.getElementById("fifth6")
+  let tot=document.getElementById("total6")
+
+tot.innerHTML=parseInt(x1.value)+parseInt(x2.value)+parseInt(x3.value)+parseInt(x4.value)+parseInt(x5.value)
+}
